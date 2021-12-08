e2ec4477705e52e35b8959530c3f45f2e7ed969f
Danu Oprea <opreadanu553@gmail.com>
2021-12-08 15:54:23 +0200

    Lab 5/6 Fixed Bug, ajax validation send data

--- AllOne/js/validation.js
+++ AllOne/js/validation.js
@@ -48,6 +48,31 @@ $(document).ready(function () {
         },
         
         submitHandler: function(form) {
+            form.submit();
+            console.log("Test");
+            let input_email = $("#sign-email").val();
+            let input_psw = $("#sign-password").val();
+            $.ajax({
+                type: 'POST',
+                url: 'php/signin.php',
+                data:{
+                    input_email: input_email,
+                    input_psw: input_psw
+                },
+                dataType: "json",
+                success: function(response){
+                    console.log("Succes!!");
+
+                    if(response.statusCode == 200){
+                        alert("Succes login");
+                        location.replace("http://localhost/AllOne/main.html");
+                    } else if(response.statusCode == 201){
+                        alert("Error: Faild login");
+                    }
+
+                }
+            })
+            
         }
     });
 
@@ -95,7 +120,35 @@ $(document).ready(function () {
         
         submitHandler: function(form) {
             form.submit();
+            let reg_fname = $("#reg-fname").val();
+            let reg_lname = $("#reg-lname").val();
+            let reg_username = $("#reg-username").val();
+            let reg_email = $("#reg-email").val();
+            let reg_psw = $("#reg-password").val();
+            let reg_pswCheck = $("#reg-password-check").val();
+            $.ajax({
+                type: 'POST',
+                url: 'php/register.php',
+                data:{
+                    reg_fname: reg_fname,
+                    reg_lname: reg_lname,
+                    reg_username: reg_username,
+                    reg_email: reg_email,
+                    reg_psw: reg_psw,
+                    reg_pswCheck: reg_pswCheck
+
+                },
+                dataType: "json",
+                success: function(response){
+                    if(response.statusCode == 200){
+                        alert("Succes register");
+                    } else if(response.statusCode == 201){
+                        alert("Error: Faild register");
+                    }
+                    
+                }
+            })
+            
         }
     });
 });
-
